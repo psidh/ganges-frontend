fd9f5291a3dfebb44b90d2b74201995ef034c0d7
P SIDHARTH <philkhanasidharth14@gmail.com>
2025-05-16 10:42:48 +0530

    meta data updated
    Signed-off-by: P SIDHARTH <philkhanasidharth14@gmail.com>

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { Plus_Jakarta_Sans } from "next/font/google";
+
 const plusJakarta = Plus_Jakarta_Sans({
   subsets: ["latin"],
 });
@@ -9,13 +10,28 @@ export const metadata: Metadata = {
   title: "Ganges",
   description:
     "A programming language — inspired by Sanskrit, built for clarity, simplicity, and fun.",
+  openGraph: {
+    title: "Ganges",
+    description:
+      "A programming language — inspired by Sanskrit, built for clarity, simplicity, and fun.",
+    url: "https://ganges.psidharth.dev",
+    siteName: "Ganges",
+    images: [
+      {
+        url: "https://ganges.psidharth.dev/assets/meta-header.png",
+        width: 1200,
+        height: 630,
+        alt: "Ganges Preview Image",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
 };
 
 export default function RootLayout({
   children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+}: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en">
       <body className={`${plusJakarta.className} antialiased`}>{children}</body>
