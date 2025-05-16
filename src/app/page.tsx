03a42f86dd7b365b9192b8a7ddfe359a0647bf4f
P SIDHARTH <philkhanasidharth14@gmail.com>
2025-05-16 10:32:25 +0530

    init
    Signed-off-by: P SIDHARTH <philkhanasidharth14@gmail.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,97 @@
-import Image from "next/image";
+import { FaPlay } from "react-icons/fa";
+import { FaComputerMouse } from "react-icons/fa6";
+import { FaGithub } from "react-icons/fa6";
 
-export default function Home() {
+export default function Page() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+    <>
+      <div
+        style={{
+          backgroundImage: 'url("/assets/bg-header.jpeg")',
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          height: "100vh",
+          width: "100vw",
+        }}
+        className="flex flex-col justify-center items-start px-32"
+      >
+        <h1 className="-ml-2 text-9xl font-medium text-black drop-shadow-lg">
+          Ganges
+        </h1>
+        <p className="mt-16 font-normal text-xl text-neutral-600 max-w-2xl">
+          A programming language — inspired by Sanskrit, built for clarity,
+          simplicity, and fun.
+        </p>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
+        <div className="mt-10 flex gap-6">
           <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+            rel="noopener"
+            href="/demo"
+            className="px-8 cursor-pointer py-4 border border-neutral-300 bg-white text-black font-semibold rounded-2xl hover:shadow-xl shadow-xl transition-all duration-300 gap-2 flex items-center"
           >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
+            <span>
+              <FaPlay />
+            </span>{" "}
+            Watch Demo
           </a>
           <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+            rel="noopener"
+            href="https://github.com/psidh/Ganges"
             target="_blank"
-            rel="noopener noreferrer"
+            className="px-8 py-4 border border-neutral-700 font-semibold rounded-2xl hover:shadow-xl transition-all duration-300 group bg-black text-white gap-2 flex items-center"
           >
-            Read our docs
+            GitHub
+            <span className="">
+              <FaGithub />
+            </span>
           </a>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+      </div>
+
+      <div
+        className="relative w-full h-screen flex items-center justify-center bg-cover bg-center"
+        style={{ backgroundImage: 'url("/assets/gradient1.svg")' }}
+      >
+          <div className="absolute inset-0 bg-black opacity-50 pointer-events-none"></div>
+        <div className="relative w-full max-w-7xl mx-auto px-12 flex items-center justify-between">
+          {/* Left content */}
+          <div className="flex flex-col items-start justify-center gap-6 text-white max-w-xl">
+            <h2 className="text-7xl font-bold mb-6">Go to PlayGround</h2>
+            <div className="flex gap-6">
+              <a
+                rel="noopener"
+                href="/playground"
+                className="px-8 cursor-pointer py-4 border border-neutral-300 bg-white text-black font-semibold rounded-2xl hover:shadow-xl shadow-xl transition-all duration-300 gap-2 flex items-center"
+              >
+                <span>
+                  <FaComputerMouse />
+                </span>{" "}
+                Playground
+              </a>
+              <a
+                rel="noopener"
+                href="https://github.com/psidh/Ganges"
+                target="_blank"
+                className="px-8 py-4 border border-neutral-700 font-semibold rounded-2xl hover:shadow-xl transition-all duration-300 group bg-black text-white gap-2 flex items-center"
+              >
+                GitHub
+                <span className="">
+                  <FaGithub />
+                </span>
+              </a>
+            </div>
+          </div>
+
+          {/* Right image */}
+          <div className="hidden lg:block">
+            <img
+              src="/assets/code1.png"
+              alt="Illustration"
+              className="w-[500px] h-auto object-cover bg-[#282A35] p-4 rounded-2xl saturate-0 hover:saturate-100 transition-all duration-300"
+            />
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
